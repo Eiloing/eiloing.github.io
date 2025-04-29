a0646bf6b6d6b915a677b8881eb4c9931e07f0ed
Eiloing <1090780183@qq.com>
2025-04-29 21:51:32 +0800

    update

---
+++ content/index.md
@@ -0,0 +1,6 @@
+---
+title: Eiloing
+---
+
+This is a blank Quartz installation.
+See the [documentation](https://quartz.jzhao.xyz) for how to get started.
--- quartz.config.ts
+++ quartz.config.ts
@@ -29,7 +29,18 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          // --primary-100:#d4eaf7;
+          // --primary-200:#b6ccd8;
+          // --primary-300:#3b3c3d;
+          // --accent-100:#71c4ef;
+          // --accent-200:#00668c;
+          // --text-100:#1d1c1c;
+          // --text-200:#313d44;
+          // --bg-100:#fffefb;
+          // --bg-200:#f5f4f1;
+          // --bg-300:#cccbc8;
+            
+          light: "##f5f4f1",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
